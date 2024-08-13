96b15b2bd25918e8d52598e276fd89e293cfcdad
muhonja <linahmuhonjaimbari@gmail.com>
2024-08-13 08:45:22 +0300

    add DashboardPage

---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,231 @@
+<script lang="ts" setup>
+import { useContractStore, useNotificationsStore} from '@/stores'
+import DialogModal from '@/components/toasts/DialogModal.vue'
+import { useField } from 'vee-validate'
+import { ref, watch } from 'vue'
+
+
+
+const contractStore = useContractStore()
+const notificationsStore = useNotificationsStore()
+const addContractEmail = () => {
+  contractStore.openAddEmailDialog()
+}
+
+const contractEmail = ref('')
+
+const emailValidator =(value: string) => {
+  if (!value) {
+    return 'Email is required'
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@klm\.com$/;
+
+  if (!emailRegex.test(value)) {
+    return 'Email must be valid ending with @klm.com'
+  }
+
+  if (value.length > 50) {
+    return 'Email must be less than 50 characters'
+  }
+  return true
+}
+
+const {
+  value: email,
+  errorMessage: emailErrorMessage,
+  meta: emailMeta
+} = useField('contractEmail', emailValidator)
+
+
+watch(()=>contractEmail.value, (value)=>{
+  email.value = value  
+})
+
+const postEmail = () => {
+  if(emailMeta.valid && emailMeta.validated){
+    notificationsStore.addNotification('Email added successfully', 'success')
+  }
+  else{
+    return
+  }
+}
+
+const logout = () => {
+  contractStore.openLogoutDialog()
+}
+
+</script>
+
+
+<template>
+  <div class="min-h-screen flex flex-row">
+<!--    right side-->
+    <div class="relative h-screen w-64 ">
+      <div class= 'fixed px-4 py-4 w-64 bg-main-color bottom-0 top-0 inset-y-0 h-screen'>
+<!--        klm logo-->
+        <div class="">
+          <p class="text-white">Welcome Linah</p>
+        </div>
+<!--        contracts per country  section-->
+        <div class="pt-4 flex flex-col">
+          <div class="pb-4">
+            <p class="text-white font-semibold">Contracts</p>
+          </div>
+          <div class="overflow-y-auto">
+            <div class="pb-4">
+              <button class="btn btn-sm w-full justify-start">
+                <span class="material-icons-outlined">summarize</span>
+                <span>Kenya</span>
+              </button>
+            </div>
+
+          </div>
+
+
+
+          <div class="">
+            <!--          Available email address-->
+            <div class="pb-4">
+              <button class="btn btn-sm w-full justify-start">
+                <span class="material-icons-outlined">email</span>
+                <span>Email addresses</span>
+              </button>
+            </div>
+            <!--          expired contracts-->
+            <div class="relative">
+              <button class="btn btn-sm w-full justify-start">
+                <span class="material-icons-outlined">running_with_errors</span>
+                <span>Expired Contracts</span>
+              </button>
+              <div class="absolute -top-2 -right-2 bg-rose-400 rounded-full flex justify-center items-centers px-2 py-1">
+                <p class="text-xs text-white">2</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+<!--        bottom section-->
+        <div class="absolute space-y-2 bottom-0 pb-4 flex flex-col w-full">
+<!--          settings section-->
+<!--          <div class="me-8">-->
+<!--            <button class="btn btn-sm w-full justify-start">-->
+<!--              <span class="material-icons-outlined">settings</span>-->
+<!--              <span>Settings</span>-->
+<!--            </button>-->
+<!--          </div>-->
+<!--Add email-->
+          <div class="me-8">
+            <button
+              @click="addContractEmail"
+              class=" btn btn-sm w-full justify-start">
+              <span class="material-icons-outlined">add</span>
+              <span>Add Email</span>
+            </button>
+          </div>
+
+          <!--          logout button-->
+          <div class="me-8">
+            <button class=" btn btn-sm w-full justify-start"
+            @click="logout">
+              <span class="material-icons-outlined">logout</span>
+              <span>Log out</span>
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+<!--    right side-->
+    <div class="flex flex-col">
+      <div class="">
+        <p>Hello</p>
+        <div><hr class="text-2xl! text-red-500"></div>
+      </div>
+      <div>
+<!--        <RouterView #default="{route, Component}">-->
+<!--          <template>-->
+<!--            <component :is="Component" :key="route.fullPath" />-->
+<!--          </template>-->
+<!--        </RouterView>-->
+        <p>Hi</p>
+      </div>
+
+    </div>
+
+  </div>
+  <DialogModal :is-open="contractStore.isEmailDialogOpen.isOpen" @close-modal="contractStore.closeEmailDialog">
+    <template #title>
+      <div class="flex justify-end ">
+        <button class="btn btn-sm btn-ghost btn-circle"
+        @click="contractStore.closeEmailDialog()">
+          <span class="material-icons-outlined">close</span>
+        </button>
+      </div>
+    </template>
+    <template #body>
+      <div class="grid grid-cols-1 gap-3 py-1 px-5">
+        <div class="flex flex-col space-y-2">
+          <label class="label text-xs font-semibold" for="email"> Email address </label>
+          <input
+            id="page-name"
+            v-model="contractEmail"
+            :class="{
+            'input-error': emailMeta.validated && !emailMeta.valid,
+            'input-primary': emailMeta.validated && emailMeta.valid
+          }"
+            class="input input-primary input-bordered w-full text-sm"
+            placeholder="Contract Email"
+            type="text"
+          />
+          <small v-if="emailMeta.validated && !emailMeta.valid" class="text-xs text-rose-500">
+            {{ emailErrorMessage }}
+          </small>
+          <small class="text-xs text-gray-500"
+          >This will be used as email address for contract expiry notification and it shall be sent 3 months prior to the expiry date
+          </small>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <div class="px-5">
+        <button
+          @click= "postEmail"
+          class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+          type="button"
+        >
+<!--          <span class="loading loading-spinner loading-sm"></span>-->
+          <span>Add Email</span>
+        </button>
+      </div>
+    </template>
+  </DialogModal>
+  <DialogModal
+    :is-open="contractStore.isLogoutDialogOpen.isOpen"
+    @closeModal="contractStore.closeLogoutDialog"
+  >
+    <template #title>
+      <div class="flex justify-center">
+        <span class="material-icons-outlined !text-6xl"> logout </span>
+      </div>
+    </template>
+    <template #body>
+      <div class="flex justify-center">
+        <h1 class="text-xl font-bold">Oh no! You're leaving...</h1>
+      </div>
+      <div class="flex justify-center">
+        <p class="text-lg font-semibold">Are you sure?</p>
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex justify-center w-full flex-row">
+        <button class="btn bg-sky-200 me-5">Sign Out</button>
+        <button class="btn bg-main-color " @click="contractStore.closeLogoutDialog()">
+          Cancel
+        </button>
+      </div>
+    </template>
+  </DialogModal>
+</template>
