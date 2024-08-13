9dfa4ea8fc9847fd98b07734213c1a857b73c868
muhonja <linahmuhonjaimbari@gmail.com>
2024-08-13 08:46:15 +0300

    add notification template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import  { useNotificationsStore} from '@/stores'
+import ToastContainer from '@/components/toasts/ToastContainer.vue'
+import ToastAlert from '@/components/toasts/ToastAlert.vue'
+
+const notificationsStore = useNotificationsStore()
 </script>
 
 <template>
-  <p class="text-rose-500">Hello</p>
+<!--  <p>Hello</p>-->
   <RouterView />
+
+  <teleport to="body">
+    <ToastContainer v-if="notificationsStore.hasNotifications">
+      <template v-for="notification in notificationsStore.getNotifications" :key="notification.id">
+        <ToastAlert
+          v-if="notification.id && notification.isShown"
+          :id="notification.id"
+          :is-shown="notification.isShown"
+          :message="notification.message"
+          :type="notification.type"
+        />
+      </template>
+    </ToastContainer>
+  </teleport>
 </template>
 
