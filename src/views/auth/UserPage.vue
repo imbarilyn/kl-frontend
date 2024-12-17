9c00154d44630d3ad43baedbd19481279a7234f1
muhonja <linahmuhonjaimbari@gmail.com>
2024-12-17 17:00:09 +0300

    add  UserPage for authentication

---
+++ src/views/auth/UserPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <RouterView #default="{Component, route}">
+    <Transition mode="out-in" appear name="">
+      <template v-if="Component">
+        <component  :is="Component" :key="route.fullPath"/>
+      </template>
+
+    </Transition>
+  </RouterView>
+
+</template>
+
+<style scoped>
+
+</style>
