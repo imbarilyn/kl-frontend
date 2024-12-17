e5f28ff2ef60a626f76f179070ad917c45044c0d
muhonja <linahmuhonjaimbari@gmail.com>
2024-12-17 17:01:55 +0300

    add  LoadingSpinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <span class="loading loading-spinner loading-lg text-AF-500"></span>
+</div>
+</template>
+
+<style scoped>
+
+</style>
