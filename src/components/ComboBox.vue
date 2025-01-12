0e8d27c0283b8fae892aeaa168c59ee9d1140898
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-01-12 08:49:00 +0300

    add prompt props and placeholder

--- src/components/ComboBox.vue
+++ src/components/ComboBox.vue
@@ -10,15 +10,6 @@ import {
 } from '@headlessui/vue'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
 
-// const people = [
-//   { id: 1, name: 'Wade Cooper' },
-//   { id: 2, name: 'Arlene Mccoy' },
-//   { id: 3, name: 'Devon Webb' },
-//   { id: 4, name: 'Tom Cook' },
-//   { id: 5, name: 'Tanya Fox' },
-//   { id: 6, name: 'Hellen Schmidt' },
-// ]
-
 interface ComboOptions {
   id: number
   name: string
@@ -26,14 +17,15 @@ interface ComboOptions {
 
 interface ComboboxProps {
   comboProps: ComboOptions[]
+  prompt?: string
 }
 
 const props = defineProps<ComboboxProps>()
 // let selected = ref(people[0])
-let selected = ref(props.comboProps[0].name)
+let selected = ref(props.prompt)
 let query = ref('')
 
-let filteredPeople = computed(() =>
+let filteredOptions = computed(() =>
   query.value === ''
     ? props.comboProps
     : props.comboProps.filter((option) =>
@@ -54,9 +46,6 @@ watch(()=>selected.value, (value)=>{
   }
 })
 
-const getLists = ()=>{
-  console.log('Hello there!!')
-}
 </script>
 
 
@@ -71,7 +60,7 @@ const getLists = ()=>{
         >
           <ComboboxInput
             class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-
+            placeholder="Search..."
             @change="query = $event.target.value"
           />
           <ComboboxButton
@@ -93,14 +82,14 @@ const getLists = ()=>{
             class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
           >
             <div
-              v-if="filteredPeople.length === 0 && query !== ''"
+              v-if="filteredOptions.length === 0 && query !== ''"
               class="relative cursor-default select-none px-4 py-2 text-gray-700"
             >
               Nothing found.
             </div>
 
             <ComboboxOption
-              v-for="option in filteredPeople"
+              v-for="option in filteredOptions"
               as="template"
               :key="option.id"
               :value="option.name"
