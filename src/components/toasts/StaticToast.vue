91e22cc390f5af36d5d90f9d5158a5ddff88220a
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-01-16 19:12:33 +0300

    add StaticToast component

---
+++ src/components/toasts/StaticToast.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  message: string
+  show: boolean
+}>()
+const emits = defineEmits<{
+  (event: 'closeStaticToast'): void
+}>()
+
+const closeStaticToast = () => {
+  emits('closeStaticToast')
+}
+</script>
+
+<template>
+  <div class="toast toast-end" v-if="props.show">
+    <div class="alert alert-info">
+      <span>{{props.message}}</span>
+      <span class="material-icons-outlined btn btn-sm btn-ghost pt-1" @click="closeStaticToast">close</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
