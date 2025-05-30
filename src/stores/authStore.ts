08dec58ec190a8c3ff7a17b40eeae73f9bfa6c7d
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-05-30 08:02:42 +0300

    add forgotPassword function

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,12 +1,11 @@
 import { defineStore } from 'pinia'
-import {useStorage} from '@vueuse/core'
-import { computed} from 'vue'
+import {resolveUnref, useStorage} from '@vueuse/core'
+import { computed, ref } from 'vue'
 import moment from 'moment'
 import { jwtDecode, type JwtPayload } from 'jwt-decode'
 
 
 interface LoginPayload {
-  username: string
   email: string
   password: string
 }
@@ -14,6 +13,7 @@ interface LoginPayload {
 interface TokenPayload extends  JwtPayload{
   user_id: number
   email: string
+  username: string
 }
 
 interface User {
@@ -23,9 +23,15 @@ interface User {
 }
 
 interface ResetPasswordPayload {
-  token: string
+  resetToken: string
   password: string
   confirmPassword: string
+}
+
+interface AuthenticationError {
+  isError: boolean
+  message: string
+  type:  'error' | 'warning' | 'info' | 'success'
 }
 
 const BASEURL = import.meta.env.VITE_BASE_URL
@@ -36,6 +42,11 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
   const tokenExpiry= useStorage('af-token-expiry', '')
   const userInfo = useStorage('af-user', '')
   // const isLoggedIn = ref<boolean>(false)
+  const isAuthenticationError = ref<AuthenticationError>({
+    isError: false,
+    message: '',
+    type: 'success'
+  })
 
   const getUserInfo = () => {
     try{
@@ -48,7 +59,7 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
 
   const isUserLoggedIn = computed(()=>{
     const now = moment().utc()
-    const expiry = moment.unix(Number(tokenExpiry.value))
+    const expiry = moment.unix(Number(tokenExpiry.value)).utc()
     const isValid = token.value && moment(now).isBefore(expiry)
     if(!isValid){
       logout()
@@ -62,20 +73,18 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
     })
   }
 
-
-  
   const setToken = (tokenData: string) => {
     token.value = tokenData
   }
 
   const decodeToken = (token: string) =>{
     const decode = jwtDecode<TokenPayload>(token)
-    const {sub, user_id, email, exp} = decode
+    const {sub, user_id, username, exp} = decode
     tokenExpiry.value = exp?.toString()
     setToken(token)
     setUserInfo({
-      username: sub as string,
-      email: email as string,
+      username: username as string,
+      email: sub as string,
       user_id: user_id as number
     })
   }
@@ -85,7 +94,6 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
     console.log(loginPayload)
     const formData = new FormData()
     formData.append('email', loginPayload.email)
-    formData.append('username',  loginPayload.username)
     formData.append('password', loginPayload.password)
     try {
       const response = await fetch(`${BASEURL}/auth/token`, {
@@ -94,39 +102,46 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
         mode: 'cors'
       }
       )
-     const resp = await response.json()
-      console.log(resp)
-      decodeToken(resp.access_token)
-      return {
-        result: 'success'
+      console.log('try block')
+      if(!response.ok){
+        return {
+          result: 'fail',
+        }
+      }
+      else{
+        const resp = await response.json()
+        decodeToken(resp.access_token)
+        return {
+          result: 'success'
+        }
       }
     }
-
-    catch{
-      return {
-        result: 'fail'
-      }
+    catch(e) {
+      // return
     }
   }
 
   async  function resetPassword(resetPasswordPayload: ResetPasswordPayload) {
     console.log(resetPasswordPayload)
     const formData = new FormData()
-    formData.append('token', resetPasswordPayload.token)
-    formData.append('password', resetPasswordPayload.password)
+    formData.append('reset_token', resetPasswordPayload.resetToken)
+    formData.append('new_password', resetPasswordPayload.password)
     formData.append('confirm_password', resetPasswordPayload.confirmPassword)
     try {
       const response = await fetch(`${BASEURL}/auth/reset-password`, {
-        method: 'PUT',
+        method: 'POST',
         mode: 'cors',
         body: formData
       })
+      console.log('try block')
       if (!response.ok) {
+        console.log('response not okay')
         return {
           result: 'fail',
-          message: 'Something went wrong try again'
+          message: 'Something went wrong, please try again'
         }
       } else {
+        console.log('response okay')
         const res = await response.json()
         console.log(res)
         return {
@@ -135,9 +150,41 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
         }
       }
     } catch (e) {
+      console.log('catch block')
       return {
         result: 'fail',
-        message: 'Password reset failed'
+        message: 'Something went wrong, please try again'
+      }
+    }
+  }
+
+  async function forgotPassword(email: string) {
+    console.log(email)
+    const formData = new FormData()
+    formData.append('email', email)
+    try {
+      const response = await fetch(`${BASEURL}/auth/forgot-password`, {
+        method: 'POST',
+        body: formData,
+        mode: 'cors'
+      })
+      if (!response.ok) {
+        return {
+          result: 'fail',
+          message: 'An error occurred while trying to reset password, please try again'
+        }
+      } else {
+        const resp = await response.json()
+        return {
+          result: resp.result,
+          message: resp.message
+        }
+      }
+    }
+    catch(e){
+      return{
+        result: 'fail',
+        message: 'An error occurred while trying to reset password, please try again'
       }
     }
   }
@@ -147,6 +194,14 @@ export  const useAuthStore = defineStore('useAuthStore', () =>{
     userInfo.value = null
   }
 
+  const closeBanner = () => {
+    isAuthenticationError.value = {
+      isError: false,
+      message: '',
+      type: 'success'
+    }
+  }
+
 return{
     token,
   userInfo,
@@ -154,6 +209,9 @@ return{
   loginUser,
   getUserInfo,
   logout,
-  resetPassword
-  }
+  resetPassword,
+  forgotPassword,
+  isAuthenticationError,
+  closeBanner
+}
 })
